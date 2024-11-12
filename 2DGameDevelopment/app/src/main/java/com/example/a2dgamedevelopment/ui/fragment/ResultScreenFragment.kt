54d292312014e1ed0549be74100082533fea2e69
Gorur56 <sevim.gorur01@gmail.com>
2024-11-12 17:04:09 +0300

    create fragment struct

---
+++ 2DGameDevelopment/app/src/main/java/com/example/a2dgamedevelopment/ui/fragment/ResultScreenFragment.kt
@@ -0,0 +1,16 @@
+package com.example.a2dgamedevelopment.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.a2dgamedevelopment.R
+
+class ResultScreenFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_result_screen, container, false)
+    }
+
+}
